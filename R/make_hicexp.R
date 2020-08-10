b318830320d8a8e31770b3d6c77d62b65558782e
sjquon <sjquon@ucsd.edu>
2020-08-10 14:45:46 -0700

    Update make_hicexp.R

--- R/make_hicexp.R
+++ R/make_hicexp.R
@@ -1,3 +1,4 @@
+#Edits from Monash for joining data.table instead of dlpyr
 #' Make Hi-C experiment object from data
 #' 
 #' @export
@@ -79,7 +80,6 @@
 
 
 make_hicexp <- function(..., data_list = NA, groups, covariates = NULL, 
-                        remove_zeros = FALSE,
                         zero.p = 0.8, A.min = 5, filter = TRUE, remove.regions = hg19_cyto) {
   if (!is.na(data_list[1])) {
     tabs <- data_list
@@ -99,31 +99,43 @@ make_hicexp <- function(..., data_list = NA, groups, covariates = NULL,
       return(new.x)
     })
   }
-  # set column names of data 
-  tabs <- lapply(tabs, function(x) {
-    colnames(x) <- c('chr', 'region1', 'region2', 'IF') 
-    return(x)
-  })
-  
-  # initialize values
-  # hic_table <- list() # initialize list for tables for each condition
-  num_per_group <- table(groups)
-  uniq_groups <- unique(groups)
-  res <- vector(length = length(uniq_groups))
   
   # convert groups to a factor if it is not already
   if (!is.factor(groups)) {
     groups <- as.factor(groups)
   }
+
+  # initialize values
+  grp_tbl <- table(groups)
+  grp_singletons <- names(grp_tbl[grp_tbl <= 1])
+  grp_logical <- groups %in% grp_singletons
+  groups <- droplevels(groups[!grp_logical])
+
+  if(length(grp_singletons) > 0) {
+    grp_str <- paste(grp_singletons, collapse = "\n")
+    warning(paste0("These groups have been dropped since they don't have replicates:\n", grp_str))
+  }
+
+  uniq_groups <- unique(groups)
+  res <- vector(length = length(uniq_groups))
   
+  #keep samples that have replicates
+  tabs <- tabs[!grp_logical]
+
+  # set column names of data and cast data.table
+  tabs <- lapply(1:length(tabs), function(i) {
+                     colnames(tabs[[i]]) <- c("chr", "region1", "region2", paste0("IF", i))
+                     as.data.table(tabs[[i]]) })
+
   # check for correct input
   if (length(groups) != length(tabs)) {
     stop("Length of groups must equal the number of Hi-C data objects entered")
   }
-  if (sum(num_per_group < 2) > 0) {
-    warning("Each experimental condition should have at least 2 samples. 
-            If you have less than 2 samples per group use HiCcompare instead")
-  }
+  #NOTE this is similar to what I did above with grp_singletons I think..
+  #if (sum(grp_tbl < 2) > 0) {
+  #  warning("Each experimental condition should have at least 2 samples. 
+  #          If you have less than 2 samples per group use HiCcompare instead")
+  #}
   if (!is.null(covariates)) {
     # check for data.frame
     if (!is(covariates, "data.frame")) {
@@ -142,61 +154,39 @@ make_hicexp <- function(..., data_list = NA, groups, covariates = NULL,
   }
   
   # cycle through groups to create a table for each experimental condition
-  
-  tmp_table <- dplyr::full_join(tabs[[1]], tabs[[2]], 
-                                by = c('chr' = 'chr', 'region1' = 'region1',
-                                       'region2' = 'region2'))
-  if (length(tabs) > 2) {
-    for(j in 3:length(tabs)) {
-      tmp_table <- dplyr::full_join(tmp_table, tabs[[j]], 
-                                    by = c('chr' = 'chr', 
-                                           'region1' = 'region1',
-                                           'region2' = 'region2'))
-    }
-  }
-  # rename table columns & replace NA IFs with 0
-  colnames(tmp_table)[4:ncol(tmp_table)] <- paste0("IF", 1:(ncol(tmp_table) - 3))
-  tmp_table[is.na(tmp_table)] <- 0
-  
+  print("CUSTOM")
+  hic_tbl <- Reduce(function(x, y) { merge.data.table(x, y, by = c("chr", "region1", "region2"), no.dups = FALSE, all = TRUE) }, tabs)
+  hic_tbl[is.na(hic_tbl)] <- 0
   
   # calculate resolution
-  bins <- unique(c(tmp_table$region1, tmp_table$region2))
+  bins <- unique(c(hic_tbl$region1, hic_tbl$region2))
   bins <- bins[order(bins)]
   resolution <- min(diff(bins))
-  tmp_table <- data.table::as.data.table(tmp_table)
-  tmp_table[, `:=`(D, abs(region2 - region1) / resolution)]
+  hic_tbl[, `:=`(D, abs(region2 - region1) / resolution)]
+
   # rearrange columns
-  tmp_table <- tmp_table[, c('chr', 'region1', 'region2', 'D',
-                             paste0("IF", 1:(ncol(tmp_table) - 4))), with = FALSE]
+  setcolorder(hic_tbl, c('chr', 'region1', 'region2', 'D'))
   
-  # set table in place on condition list
-  hic_table <- tmp_table
   # check chr format, if "chr#" change to just the number
-  if (!is.numeric(hic_table$chr)) {
+  if (!is.numeric(hic_tbl$chr)) {
     # replace any "chr"
-    hic_table[, chr := sub("chr", "", chr)]
+    hic_tbl[, chr := sub("chr", "", chr)]
     # replace any X 
-    hic_table[, chr := sub("X", "23", chr)]
+    hic_tbl[, chr := sub("X", "23", chr)]
     # replace any Y
-    hic_table[, chr := sub("Y", "24", chr)]
+    hic_tbl[, chr := sub("Y", "24", chr)]
     # convert to numeric
-    hic_table[, chr := as.numeric(chr)]
+    hic_tbl[, chr := as.numeric(chr)]
   }
-  # sort hic_table
-  hic_table <- hic_table[order(chr, region1, region2),]
+  # sort hic_tbl
+  #TODO do we really need to do this ? I wonder if it would be faster to do it on individual samples
+  #  and then outer join. I think outer join should preserve the sorting order..
+  setorder(hic_tbl, chr, region1, region2)
   
   # check that all resolutions are equal
   if (length(unique(res)) > 1) {
     stop("Resolution of all datasets must be equal.")
   }
-  # remove rows with 0's
-  if (remove_zeros) {
-    IFs <- as.matrix(hic_table[, -c("chr", "region1", "region2", "D"), with = FALSE])
-    IFs <- IFs == 0 # matrix of TRUE where zeros occur
-    keep <- rowSums(IFs) == 0 # if row has no zeros it will sum to 0
-    hic_table <- hic_table[keep,]
-  }
-  
   
   # make metadata 
   metadata <- data.frame(group = groups)
@@ -206,15 +196,19 @@ make_hicexp <- function(..., data_list = NA, groups, covariates = NULL,
   }
   
   # put into hicexp object
-  experiment <- new("Hicexp", hic_table = hic_table, 
+  experiment <- new("Hicexp", hic_table = hic_tbl, 
                     comparison = data.table::data.table(), 
                     metadata = metadata, resolution = resolution, 
                     normalized = FALSE)
   
-  # filter
-  if (filter) {
-    experiment <- hic_filter(experiment, zero.p = zero.p, A.min = A.min, remove.regions = remove.regions)
+  # filtering steps
+  if(filter) {
+    experiment <- hic_filter(experiment, zero.p = zero.p, A.min = A.min)
   }
   
-  return(experiment)
+  if(!is.null(remove.regions)) {
+    experiment <- hic_filter_regions(experiment, remove.regions = remove.regions)
   }
+
+  return(experiment)
+}
